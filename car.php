e053bb29fe08bfc52a4dfd7f3dc76971004f97ed
Calla Rudolph and Rena Rallis <calla@>
2017-06-26 09:38:22 -0700

    encapsulate Car class with each property getter/setter

--- car.php
+++ car.php
@@ -1,40 +1,80 @@
 <?php
-class Car
-{
-    public $make_model;
-    public $price;
-    public $miles;
-}
+    class Car
+    {
+        private $make_model;
+        private $price;
+        private $miles;
+        function worthBuying($max_price)
+           {
+               return $this->price < ($max_price + 100);
+           }
 
+        function __construct($car_model, $car_price, $car_miles)
+        {
+            $this->make_model = $car_model;
+            $this->price = $car_price;
+            $this->miles = $car_miles;
+        }
 
-$porsche = new Car();
-$porsche->make_model = "2014 Porsche 911";
-$porsche->price = 114991;
-$porsche->miles = 7864;
+        function setModel($new_model)
+        {
+            $this->make_model = (float) $new_model;
+            // if ($float_model != 0) {
+            //   $this->model = $float_model;
+            // }
+        }
 
-$ford = new Car();
-$ford->make_model = "2011 Ford F450";
-$ford->price = 55995;
-$ford->miles = 14241;
+        function getModel()
+        {
+            return $this->make_model;
+        }
 
-$lexus = new Car();
-$lexus->make_model = "2013 Lexus RX 350";
-$lexus->price = 44700;
-$lexus->miles = 20000;
+        function setPrice($new_price)
+        {
+            $float_price = (float) $new_price;
+            if ($float_price != 0) {
+              $formatted_price = number_format($float_price, 2);
+              $this->price = $float_price;
+            }
+        }
 
-$mercedes = new Car();
-$mercedes->make_model = "Mercedes Benz CLS550";
-$mercedes->price = 39900;
-$mercedes->miles = 37979;
+        function getPrice()
+        {
+            return $this->price;
+        }
 
-$cars = array($porsche, $ford, $lexus, $mercedes);
+        function setMiles($new_miles)
+        {
+            $float_miles = (float) $new_miles;
+            if ($float_miles != 0) {
+              $formatted_miles = number_format($float_miles, 2);
+              $this->miles = $float_miles;
+            }
+        }
 
-$cars_matching_search = array();
-foreach ($cars as $car) {
-    if ($car->price < $_GET["price"]) {
-        array_push($cars_matching_search, $car);
+        function getMiles()
+        {
+            return $this->miles;
+        }
     }
-}
+
+    $porsche = new Car("2014 Porsche 911", 114991, 7864);
+    $ford = new Car("2011 Ford F450", 55995, 14241);
+    $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+
+    $porsche->setModel("2013 Porsche");
+    $ford->setPrice(15000);
+    $lexus->setMiles(40000);
+
+    $cars = array($porsche, $ford, $lexus, $mercedes);
+
+    $cars_matching_search = array();
+    foreach ($cars as $car) {
+        if ($car->worthBuying($_GET['price'])) {
+            array_push($cars_matching_search, $car);
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -46,10 +86,13 @@ foreach ($cars as $car) {
     <ul>
         <?php
             foreach ($cars_matching_search as $car) {
-                echo "<li> $car->make_model </li>";
+                $matching_model = $car->getModel();
+                $matching_price = $car->getPrice();
+                $matching_miles = $car->getMiles();
+                echo "<li> $matching_model </li>";
                 echo "<ul>";
-                    echo "<li> $$car->price </li>";
-                    echo "<li> Miles: $car->miles </li>";
+                    echo "<li> $$matching_price </li>";
+                    echo "<li> Miles: $matching_miles </li>";
                 echo "</ul>";
             }
         ?>
