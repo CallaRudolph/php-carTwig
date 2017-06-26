f674d628e728e0eb2bf6b35fdbe39b97a60896ac
Calla Rudolph and Rena Rallis <calla@>
2017-06-26 09:48:44 -0700

    add image to each car

--- car.php
+++ car.php
@@ -4,24 +4,23 @@
         private $make_model;
         private $price;
         private $miles;
+        private $image;
         function worthBuying($max_price)
            {
                return $this->price < ($max_price + 100);
            }
 
-        function __construct($car_model, $car_price, $car_miles)
+        function __construct($car_model, $car_price, $car_miles, $car_image)
         {
             $this->make_model = $car_model;
             $this->price = $car_price;
             $this->miles = $car_miles;
+            $this->image = $car_image;
         }
 
         function setModel($new_model)
         {
-            $this->make_model = (float) $new_model;
-            // if ($float_model != 0) {
-            //   $this->model = $float_model;
-            // }
+            $this->make_model = $new_model;
         }
 
         function getModel()
@@ -56,16 +55,27 @@
         {
             return $this->miles;
         }
+
+        function setImage($new_image)
+        {
+            $this->image = $new_image;
+        }
+
+        function getImage()
+        {
+            return $this->image;
+        }
     }
 
-    $porsche = new Car("2014 Porsche 911", 114991, 7864);
-    $ford = new Car("2011 Ford F450", 55995, 14241);
-    $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
-    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+    $porsche = new Car("2014 Porsche 911", 114991, 7864, "images/porsche.jpg");
+    $ford = new Car("2011 Ford F450", 55995, 14241, "images/ford.jpeg");
+    $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "images/lexus.jpeg");
+    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "images/mercedes1.jpeg");
 
     $porsche->setModel("2013 Porsche");
     $ford->setPrice(15000);
     $lexus->setMiles(40000);
+    $mercedes->setImage("images/mercedes2.jpeg");
 
     $cars = array($porsche, $ford, $lexus, $mercedes);
 
@@ -89,10 +99,12 @@
                 $matching_model = $car->getModel();
                 $matching_price = $car->getPrice();
                 $matching_miles = $car->getMiles();
+                $matching_image = $car->getImage();
                 echo "<li> $matching_model </li>";
                 echo "<ul>";
                     echo "<li> $$matching_price </li>";
                     echo "<li> Miles: $matching_miles </li>";
+                    echo "Pic: <img src=$matching_image>";
                 echo "</ul>";
             }
         ?>
