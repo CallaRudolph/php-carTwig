4408763118f6469973cf18c11435fb7d21158705
Calla Rudolph, jacob ruleaux and Michelle Poterek <calla@>
2017-06-29 09:45:52 -0700

    create twig paths

--- app/app.php
+++ app/app.php
@@ -5,12 +5,25 @@
 
     session_start();
 
+    if(empty($_SESSION['list_of_cars'])) {
+        $_SESSION['list_of_cars'] = array();
+    }
+
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__.'/..views'
+        'twig.path' => __DIR__.'/../views'
     ));
 
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('car.html.twig', array('cars' => Car::getAll()));
+    });
+
+    $app->post("/create", function() use ($app) {
+        $car = new Car($_POST['make-model'], $_POST['price'], $_POST['mileage'], $_POST['image']);
+        $car->save();
+        return $app['twig']->render('create_car.html.twig', array('newcar' => $car));
+    });
 
     return $app;
 ?>
---
+++ src/Car.php
@@ -0,0 +1,69 @@
+<?php
+    class Car
+    {
+        private $make_model;
+        private $price;
+        private $miles;
+        private $image;
+
+
+        function __construct($car_model, $car_price, $car_miles, $car_image)
+        {
+            $this->make_model = $car_model;
+            $this->price = $car_price;
+            $this->miles = $car_miles;
+            $this->image = $car_image;
+        }
+
+        function setModel($new_model)
+        {
+            $this->make_model = $new_model;
+        }
+
+        function getModel()
+        {
+            return $this->make_model;
+        }
+
+        function setPrice($new_price)
+        {
+            $this->price = $new_price;
+        }
+
+        function getPrice()
+        {
+            return $this->price;
+        }
+
+        function setMiles($new_miles)
+        {
+            $this->miles = $new_miles;
+        }
+
+        function getMiles()
+        {
+            return $this->miles;
+        }
+
+        function setImage($new_image)
+        {
+            $this->image = $new_image;
+        }
+
+        function getImage()
+        {
+            return $this->image;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['list_of_cars'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_cars'];
+        }
+    }
+
+    ?>
